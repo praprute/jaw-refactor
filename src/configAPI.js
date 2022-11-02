efd5045ba3a1e959d1455185611c6068f537224e
junior <trader.junior492047@gmail.com>
2022-11-02 23:02:39 +0700

    convert to veithong

--- src/configAPI.js
+++ src/configAPI.js
@@ -1,8 +1,8 @@
 export const API = process.env.REACT_APP_APIKEY
 export const Company = {
-  Name: `VIET HUONG CO., LTD.`,
-  Address: `89 Moo 4, Makamku, Nikhom Phatthana, Rayong 21180`,
-  Phone: `Tel: 66-38-624432 Fax: 66-38-940086`,
-  Web: ` `,
-  Email: ` `,
+  Name: `RUNGROJ FISH SAUCE CO., LTD.`,
+  Address: `8/4 Samutjadee Rd. Paknum Mueang Rayong, Rayong 21000 Thailand`,
+  Phone: `Tel: 66-38-940388 Fax: 66-38-940086`,
+  Web: `https://www.rungrojfishsauce.com`,
+  Email: `Email: fshsauce@rungrojfshsauce.com`,
 }
