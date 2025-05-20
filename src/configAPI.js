66fde01040ccfb98cfc6081d84b22eebaa0964f6
praprutepummala <praprute.pu@hitop.co.t>
2025-05-21 00:52:15 +0700

    for veit

--- src/configAPI.js
+++ src/configAPI.js
@@ -2,17 +2,6 @@
 // -------------------------------------------------------- กรณี Veit ------------------------------------------------------------------------------------
 
 
-export const API = process.env.REACT_APP_APIKEY
-export const Company = {
-  Name: `RUNGROJ FISH SAUCE CO., LTD.`,
-  Address: `8/4 Samutjadee Rd. Paknum Mueang Rayong, Rayong 21000 Thailand`,
-  Phone: `Tel: 66-38-940388 Fax: 66-38-940086`,
-  Web: `https://www.rungrojfishsauce.com`,
-  Email: `Email: fishsauce@rungrojfishsauce.com`,
-}
-
-// -------------------------------------------------------- กรณี Jaw ------------------------------------------------------------------------------------
-
 // export const API = process.env.REACT_APP_APIKEY
 // export const Company = {
 //   Name: `RUNGROJ FISH SAUCE CO., LTD.`,
@@ -21,3 +10,14 @@ export const Company = {
 //   Web: `https://www.rungrojfishsauce.com`,
 //   Email: `Email: fishsauce@rungrojfishsauce.com`,
 // }
+
+// -------------------------------------------------------- กรณี Jaw ------------------------------------------------------------------------------------
+
+export const API = process.env.REACT_APP_APIKEY
+export const Company = {
+  Name: `RUNGROJ FISH SAUCE CO., LTD.`,
+  Address: `8/4 Samutjadee Rd. Paknum Mueang Rayong, Rayong 21000 Thailand`,
+  Phone: `Tel: 66-38-940388 Fax: 66-38-940086`,
+  Web: `https://www.rungrojfishsauce.com`,
+  Email: `Email: fishsauce@rungrojfishsauce.com`,
+}
