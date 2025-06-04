1bc105a85d8a4fa939e94e77b3b65586eb115fe6
praprutepummala <praprute.pu@hitop.co.t>
2025-06-05 01:54:09 +0700

    jaw

--- src/configAPI.js
+++ src/configAPI.js
@@ -2,22 +2,22 @@
 // -------------------------------------------------------- กรณี Veit ------------------------------------------------------------------------------------
 
 
-// export const API = process.env.REACT_APP_APIKEY
-// export const Company = {
-//   Name: `RUNGROJ FISH SAUCE CO., LTD.`,
-//   Address: `8/4 Samutjadee Rd. Paknum Mueang Rayong, Rayong 21000 Thailand`,
-//   Phone: `Tel: 66-38-940388 Fax: 66-38-940086`,
-//   Web: `https://www.rungrojfishsauce.com`,
-//   Email: `Email: fishsauce@rungrojfishsauce.com`,
-// }
+export const API = process.env.REACT_APP_APIKEY
+export const Company = {
+  Name: `RUNGROJ FISH SAUCE CO., LTD.`,
+  Address: `8/4 Samutjadee Rd. Paknum Mueang Rayong, Rayong 21000 Thailand`,
+  Phone: `Tel: 66-38-940388 Fax: 66-38-940086`,
+  Web: `https://www.rungrojfishsauce.com`,
+  Email: `Email: fishsauce@rungrojfishsauce.com`,
+}
 
 // -------------------------------------------------------- กรณี Jaw ------------------------------------------------------------------------------------
 
-export const API = process.env.REACT_APP_APIKEY
-export const Company = {
-  Name: `VIET HUONG CO., LTD.`,
-  Address: `89 Moo 4, Makamku, Nikhom Phatthana, Rayong 21180`,
-  Phone: `Tel: 66-94-8562698 Fax: 66-38-940086`,
-  Web:` `,
-  Email: `   `,
-}
+// export const API = process.env.REACT_APP_APIKEY
+// export const Company = {
+//   Name: `VIET HUONG CO., LTD.`,
+//   Address: `89 Moo 4, Makamku, Nikhom Phatthana, Rayong 21180`,
+//   Phone: `Tel: 66-94-8562698 Fax: 66-38-940086`,
+//   Web:` `,
+//   Email: `   `,
+// }
